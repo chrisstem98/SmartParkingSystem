1d3f4f70611e3797931f452d8cdb7222c33b01ed
ChrisStem <chrisstemitsiotis@gmail.com>
2025-10-20 22:41:51 +0300

    Adding PostgreSQL confiq in setting.py

--- backend/smartparking/settings.py
+++ backend/smartparking/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-bexw&3(8k(jr9m8nz36b5n^uovh@==sd_%0o+15!7%y1r#)ne1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
@@ -74,8 +73,12 @@ WSGI_APPLICATION = 'smartparking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'parkingdb'),
+        'USER': os.environ.get('POSTGRES_USER', 'parkinguser'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'parkingpass'),
+        'HOST': 'db',       # Το service name από το docker-compose.yml
+        'PORT': '5432',
     }
 }
 
@@ -115,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
