b90c2b211179172d6adf18b08c341f38ff7f8c61
ChrisStem <chrisstemitsiotis@gmail.com>
2025-10-30 19:33:57 +0200

    Setting up frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-8 bg-blue-100 text-blue-800 text-3xl font-bold">
+      🚗 Tailwind is working perfectly!
+    </div>
+  );
+}
+
+export default App;
